817bb272f1ce2b7afb8cbd25e33dca375d83b404
Ivander <ivandersalvadorruiz@gmail.com>
2020-05-28 09:14:11 -0300

    Projeto criado

---
+++ src/main/java/com/salvadot/sgmfweb/SgmfWebApplication.java
@@ -0,0 +1,13 @@
+package com.salvadot.sgmfweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SgmfWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SgmfWebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/salvadot/sgmfweb/SgmfWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.salvadot.sgmfweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SgmfWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
